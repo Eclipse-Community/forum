3885f96601ffead5f01b45259ed1d40cb3a5d0fc
pixieditzy <77038953+pixieditzy@users.noreply.github.com>
2021-01-10 04:01:59 +0000

    oh

--- plugins/randomforum/forumListMangler.php
+++ plugins/randomforum/forumListMangler.php
@@ -1,10 +1,7 @@
 <?php
 
-if (!Settings::pluginGet('forumid')) continue;
 if($forum['id'] == Settings::pluginGet('forumid'))
 {
-	$rndTitles = file_get_contents("./plugins/randomforum/titles.txt");
-	$rndTitles = explode("\n", $rndTitles);
 	$rndDescs = file_get_contents("./plugins/randomforum/descs.txt");
 	$rndDescs = explode("\n", $rndDescs);
 
